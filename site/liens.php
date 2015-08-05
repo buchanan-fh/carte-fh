b152eea47562909e783e2b3cc5b755794b180a59
buchanan-fh <carte.fh@free.fr>
2015-08-05 22:45:20 +0200

    MàJ Août 2015

--- site/liens.php
+++ site/liens.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json');
 $t_start=microtime(true);
 
 $nb_ope=86;
-$tab_dates_ok=array('201501','201502','201503','201504','201505','201506','201507');
+$tab_dates_ok=array('201501','201502','201503','201504','201505','201506','201507','201508');
 $short_links = array();
 $final_links = array();
 $all_sup = array();
