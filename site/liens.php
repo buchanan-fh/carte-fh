4a6d0a2ebb74a09fdc54a511b952d0aad4608cda
buchanan-fh <carte.fh@free.fr>
2015-08-22 16:43:31 +0200

    MàJ liste des exploitants

--- site/liens.php
+++ site/liens.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 
 $t_start=microtime(true);
 
-$nb_ope=86;
+$nb_ope=88;
 $tab_dates_ok=array('201501','201502','201503','201504','201505','201506','201507','201508');
 $short_links = array();
 $final_links = array();
